e10cf2cd0f13e337c44a7db33926a2ef316d87ef
logonoff <logonoff@outlook.com>
2018-02-28 21:05:33 -0500

    fix padding in logo64+logo32

--- assets/main.js
+++ assets/main.js
@@ -12,18 +12,6 @@ function burgerButton(x) {
 	x.classList.toggle("change");
 }
 
-// error box
-$(function() {
-	$("#draggable").draggable();
-});
-
-// windows
-if (window.self !== window.top) {
-	document.getElementById('window').style.display = "none";
-};
-
-
-
 // animationals WAIT FOR CNAME 
 $("#gitbutton").click(function() {
   $('.github').toggleClass('github-active');
@@ -37,58 +25,3 @@ $("#discordbutton").click(function() {
 function delay (URL) {
     setTimeout( function() { window.location = URL }, 300 );
 }
-
-
-//smoothstate test
-(function ($) {
-    'use strict';
-    $(document).ready(function () {
-
-        // Init here.
-        var $body = $('body'),
-            $main = $('#main'),
-            $site = $('html, body'),
-            transition = 'fade',
-            smoothState;
-
-        smoothState = $main.smoothState({
-            onBefore: function($anchor, $container) {
-                var current = $('[data-viewport]').first().data('viewport'),
-                    target = $anchor.data('target');
-                current = current ? current : 0;
-                target = target ? target : 0;
-                if (current === target) {
-                    transition = 'fade';
-                } else if (current < target) {
-                    transition = 'moveright';
-                } else {
-                    transition = 'moveleft';
-                }
-            },
-            onStart: {
-                duration: 400,
-                render: function (url, $container) {
-                    $main.attr('data-transition', transition);
-                    $main.addClass('is-exiting');
-                    $site.animate({scrollTop: 0});
-                }
-            },
-            onReady: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    $container.html($newContent);
-                    $container.removeClass('is-exiting');
-                }
-            },
-            onAfter: {
-                duration: 0,
-                render: function ($container, $newContent) {
-                    $container.html($newContent);
-                    $container.removeClass('is-exiting');
-                }
-            },
-        }).data('smoothState');
-
-    });
-
-}(jQuery));
