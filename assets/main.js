5c9010a149c50c2abed315bbf16e78991de62c39
logonoff <logonoff@outlook.com>
2018-03-08 23:35:11 -0500

    add text size, contrast options

--- assets/main.js
+++ assets/main.js
@@ -3,40 +3,40 @@ function menuFocus() { document.querySelector('.burgerButton').click(); }
 function fadeoutright() { $( "#main" ).addClass( "fadeoutright" ); scrollup(); }
 function fadeoutleft() { $( "#main" ).addClass( "fadeoutleft" ); scrollup(); }
 function scrollup() { $("html, body").animate({ scrollTop: 0 }, 150); return false; }
+function textNormal() { $('html').css("font-size", "1em"); }
+function textLarge() { $('html').css("font-size", "2em"); }
 
 $(document).ready(function() {
-    var clicked = false;
+    var clickedBurger = false;
     $('.burgerButton').click(function() {
-        if(clicked) {
-            clicked = false;
+        if(clickedBurger) {
+            clickedBurger = false;
             $('.menuBar').removeClass('slideDown').addClass('slideUp').removeClass('responsive');
             $('.menuFocus').removeClass('menuFocusIn').addClass('menuFocusOut');
             setTimeout( function() { $('.menuFocus').toggleClass("active") }, 300); 
         }
         else {
-            clicked = true;
+            clickedBurger = true;
             $('.menuBar').removeClass('slideUp').addClass('slideDown').addClass('responsive');
             $('.menuFocus').removeClass('menuFocusOut').addClass('menuFocusIn').toggleClass("active");
         }
      });
+
+    var clickedContrast = false;
+    $('.contrast').click(function() {
+        if(clickedContrast) {
+            clickedContrast = false;
+            $('#contrastParent').empty(); 
+            $('#contrast').remove(); 
+        }
+        else {
+            clickedContrast = true;
+            $('head').append('<div id="contrastParent"><link rel="stylesheet" id="contrast" href="assets/contrast.css" type="text/css"/></div>');
+        }
+     });
 });
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-//W3S slideshow
+/* W3S slideshow (i know, very insecure probally)
 var slideIndex = 1; showSlides(slideIndex);
 function plusSlides(n) { showSlides(slideIndex += n); }
 function currentSlide(n) { showSlides(slideIndex = n); }
@@ -55,12 +55,13 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+*/
 
 //edgey
 var userAgent = navigator.userAgent;
 if( userAgent.indexOf('Edge') >= 0){
 	console.log("dammit edge lol");
-	$('#main').append('<link rel="stylesheet" href="assets/edgey.css" type="text/css"/>');
+	$('head').append('<link rel="stylesheet" href="assets/edgey.css" type="text/css"/>');
 } 
 
 // animationals WAIT FOR CNAME 
