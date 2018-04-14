616ed93c52ebe413f2d00432753d936a99c1dadb
logonoff <logonoff@outlook.com>
2018-04-14 18:47:56 -0400

    minify js

--- assets/main.js
+++ assets/main.js
@@ -27,8 +27,8 @@
 
 function burgerButton(x) {x.classList.toggle("change");}
 function menuFocus() {document.querySelector(".burgerButton").click();}
-// function fadeoutright() {$('#main').addClass("fadeoutright"); scrollup();}
-// function fadeoutleft() {$('#main').addClass("fadeoutleft"); scrollup();}
+function fadeoutright() {$('#main').addClass("fadeoutright"); scrollup();}
+function fadeoutleft() {$('#main').addClass("fadeoutleft"); scrollup();}
 function scrollup() {$('html, body').animate({scrollTop: 0}, 150); return false;}
 function someone() {$("body").addClass("someone");} // help i've fallen and i cant get up
 
@@ -50,11 +50,14 @@ function darkSideOfTheMoon() {
 }
 
 $(document).ready(function() {
-	$(".title").append("<span class=\"accessibilityMenu\"><a class=\"captionButton textAdjust\" onclick=\"textNormal()\">a</a> <a class=\"captionButton textAdjust\" onclick=\"textLarge()\">A</a> <a class=\"captionButton contrast\"><i class=\"fas fa-adjust\"></i></a></span>") // add accessbility
+	// add accessbility
+	$(".titleText").css("left", "2.8rem");
+	$(".title").append("<span class=\"accessibilityMenu\"><a class=\"captionButton textAdjust\" onclick=\"textNormal()\">a</a> <a class=\"captionButton textAdjust\" onclick=\"textLarge()\">A</a> <a class=\"captionButton contrast\"><i class=\"fas fa-adjust\"></i></a></span>");
+
 
 	Barba.Pjax.start(); Barba.Prefetch.init(); // init barbra
 
-	$('.tilts').tilt({maxTilt: 20,}) // init tilt.js 
+	$('.tilts').tilt({maxTilt: 20,}); // init tilt.js 
 
 	// init waves.js
 	Waves.attach('.button');
@@ -129,68 +132,6 @@ $(document).ready(function() {
 	});
 });
 
-var FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-    /**
-     * This function is automatically called as soon the Transition starts
-     * this.newContainerLoading is a Promise for the loading of the new container
-     * (Barba.js also comes with an handy Promise polyfill!)
-     */
-
-    // As soon the loading is finished and the old page is faded out, let's fade the new page
-    Promise
-      .all([this.newContainerLoading, this.fadeOut()])
-      .then(this.fadeIn.bind(this));
-  },
-
-  fadeOut: function() {
-    /**
-     * this.oldContainer is the HTMLElement of the old Container
-     */
-
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
-  },
-
-  fadeIn: function() {
-    /**
-     * this.newContainer is the HTMLElement of the new Container
-     * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-     * Please note, newContainer is available just after newContainerLoading is resolved!
-     */
-
-    var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility : 'visible',
-      opacity : 0
-    });
-
-    $el.animate({ opacity: 1 }, 400, function() {
-      /**
-       * Do not forget to call .done() as soon your transition is finished!
-       * .done() will automatically remove from the DOM the old Container
-       */
-
-      _this.done();
-    });
-  }
-});
-
-/**
- * Next step, you have to tell Barba to use the new Transition
- */
-
-Barba.Pjax.getTransition = function() {
-  /**
-   * Here you can use your own logic!
-   * For example you can use different Transition based on the current page or link...
-   */
-
-  return FadeTransition;
-};
 /* W3S slideshow (i know, very insecure probally)
 var slideIndex = 1; showSlides(slideIndex);
 function plusSlides(n) {showSlides(slideIndex += n);}
@@ -217,4 +158,4 @@ $("#gitbutton").click(function() {$('.github').toggleClass('github-active');});
 $("#discordbutton").click(function() {$('.discord').toggleClass('discord-active');});
 
 // delay links when animationals come
-// function delay (URL) {setTimeout( function() {window.location = URL;}, 50) ;}
+function delay (URL) {setTimeout( function() {window.location = URL;}, 50) ;}
