95753be026e5057a64b43c3b367893182c04a040
logonoff <logonoff@outlook.com>
2018-05-23 17:13:43 -0400

    add a button testing thing
    also now use cookies, but since they are  for user‑interface customisation, they are exempt from eu cookie law

--- assets/main.js
+++ assets/main.js
@@ -31,38 +31,32 @@
 /* mousetrap v1.6.1 craig.is/killing/mice */
 !function(e,t,n){function r(e,t,n){e.addEventListener?e.addEventListener(t,n,!1):e.attachEvent("on"+t,n)}function i(e){if("keypress"==e.type){var t=String.fromCharCode(e.which);return e.shiftKey||(t=t.toLowerCase()),t}return l[e.which]?l[e.which]:u[e.which]?u[e.which]:String.fromCharCode(e.which).toLowerCase()}function o(e){return"shift"==e||"ctrl"==e||"alt"==e||"meta"==e}function a(e,t){var n,r,i,a=[];for("+"===(n=e)?n=["+"]:n=(n=n.replace(/\+{2}/g,"+plus")).split("+"),i=0;i<n.length;++i)r=n[i],f[r]&&(r=f[r]),t&&"keypress"!=t&&p[r]&&(r=p[r],a.push("shift")),o(r)&&a.push(r);if(n=r,!(i=t)){if(!s)for(var c in s={},l)95<c&&112>c||l.hasOwnProperty(c)&&(s[l[c]]=c);i=s[n]?"keydown":"keypress"}return"keypress"==i&&a.length&&(i="keydown"),{key:r,modifiers:a,action:i}}function c(e){function n(e){e=e||{};var t,n=!1;for(t in d)e[t]?n=!0:d[t]=0;n||(k=!1)}function s(e,t,n,r,i,a){var c,s,l=[],u=n.type;if(!f._callbacks[e])return[];for("keyup"==u&&o(e)&&(t=[e]),c=0;c<f._callbacks[e].length;++c)if(s=f._callbacks[e][c],(r||!s.seq||d[s.seq]==s.level)&&u==s.action){var p;(p="keypress"==u&&!n.metaKey&&!n.ctrlKey)||(p=s.modifiers,p=t.sort().join(",")===p.sort().join(",")),p&&(p=r&&s.seq==r&&s.level==a,(!r&&s.combo==i||p)&&f._callbacks[e].splice(c,1),l.push(s))}return l}function l(e,t,n,r){f.stopCallback(t,t.target||t.srcElement,n,r)||!1!==e(t,n)||(t.preventDefault?t.preventDefault():t.returnValue=!1,t.stopPropagation?t.stopPropagation():t.cancelBubble=!0)}function u(e){"number"!=typeof e.which&&(e.which=e.keyCode);var t,n,r=i(e);r&&("keyup"==e.type&&y===r?y=!1:f.handleKey(r,(n=[],(t=e).shiftKey&&n.push("shift"),t.altKey&&n.push("alt"),t.ctrlKey&&n.push("ctrl"),t.metaKey&&n.push("meta"),n),e))}function p(e,t,r,o,c){f._directMap[e+":"+r]=t;var u=(e=e.replace(/\s+/g," ")).split(" ");1<u.length?function(e,t,r,o){function c(t){return function(){k=t,++d[e],clearTimeout(h),h=setTimeout(n,1e3)}}function s(t){l(r,t,e),"keyup"!==o&&(y=i(t)),setTimeout(n,10)}for(var u=d[e]=0;u<t.length;++u){var f=u+1===t.length?s:c(o||a(t[u+1]).action);p(t[u],f,o,e,u)}}(e,u,t,r):(r=a(e,r),f._callbacks[r.key]=f._callbacks[r.key]||[],s(r.key,r.modifiers,{type:r.action},o,e,c),f._callbacks[r.key][o?"unshift":"push"]({callback:t,modifiers:r.modifiers,action:r.action,seq:o,level:c,combo:e}))}var f=this;if(e=e||t,!(f instanceof c))return new c(e);f.target=e,f._callbacks={},f._directMap={};var h,d={},y=!1,m=!1,k=!1;f._handleKey=function(e,t,r){var i,a=s(e,t,r);t={};var c=0,u=!1;for(i=0;i<a.length;++i)a[i].seq&&(c=Math.max(c,a[i].level));for(i=0;i<a.length;++i)a[i].seq?a[i].level==c&&(u=!0,t[a[i].seq]=1,l(a[i].callback,r,a[i].combo,a[i].seq)):u||l(a[i].callback,r,a[i].combo);a="keypress"==r.type&&m,r.type!=k||o(e)||a||n(t),m=u&&"keydown"==r.type},f._bindMultiple=function(e,t,n){for(var r=0;r<e.length;++r)p(e[r],t,n)},r(e,"keypress",u),r(e,"keydown",u),r(e,"keyup",u)}if(e){var s,l={8:"backspace",9:"tab",13:"enter",16:"shift",17:"ctrl",18:"alt",20:"capslock",27:"esc",32:"space",33:"pageup",34:"pagedown",35:"end",36:"home",37:"left",38:"up",39:"right",40:"down",45:"ins",46:"del",91:"meta",93:"meta",224:"meta"},u={106:"*",107:"+",109:"-",110:".",111:"/",186:";",187:"=",188:",",189:"-",190:".",191:"/",192:"`",219:"[",220:"\\",221:"]",222:"'"},p={"~":"`","!":"1","@":"2","#":"3",$:"4","%":"5","^":"6","&":"7","*":"8","(":"9",")":"0",_:"-","+":"=",":":";",'"':"'","<":",",">":".","?":"/","|":"\\"},f={option:"alt",command:"meta",return:"enter",escape:"esc",plus:"+",mod:/Mac|iPod|iPhone|iPad/.test(navigator.platform)?"meta":"ctrl"};for(n=1;20>n;++n)l[111+n]="f"+n;for(n=0;9>=n;++n)l[n+96]=n.toString();c.prototype.bind=function(e,t,n){return e=e instanceof Array?e:[e],this._bindMultiple.call(this,e,t,n),this},c.prototype.unbind=function(e,t){return this.bind.call(this,e,function(){},t)},c.prototype.trigger=function(e,t){return this._directMap[e+":"+t]&&this._directMap[e+":"+t]({},e),this},c.prototype.reset=function(){return this._callbacks={},this._directMap={},this},c.prototype.stopCallback=function(e,n){return!(-1<(" "+n.className+" ").indexOf(" mousetrap ")||function e(n,r){return null!==n&&n!==t&&(n===r||e(n.parentNode,r))}(n,this.target))&&("INPUT"==n.tagName||"SELECT"==n.tagName||"TEXTAREA"==n.tagName||n.isContentEditable)},c.prototype.handleKey=function(){return this._handleKey.apply(this,arguments)},c.addKeycodes=function(e){for(var t in e)e.hasOwnProperty(t)&&(l[t]=e[t]);s=null},c.init=function(){var e,n=c(t);for(e in n)"_"!==e.charAt(0)&&(c[e]=function(e){return function(){return n[e].apply(n,arguments)}}(e))},c.init(),e.Mousetrap=c,"undefined"!=typeof module&&module.exports&&(module.exports=c),"function"==typeof define&&define.amd&&define(function(){return c})}}("undefined"!=typeof window?window:null,"undefined"!=typeof window?document:null);
 
-/*! modernizr 3.5.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-borderimage-cssgrid_cssgridlegacy-setclasses !*/
-!function(e,n,t){function r(e,n){return typeof e===n}function o(){var e,n,t,o,s,i,l;for(var a in w)if(w.hasOwnProperty(a)){if(e=[],n=w[a],n.name&&(e.push(n.name.toLowerCase()),n.options&&n.options.aliases&&n.options.aliases.length))for(t=0;t<n.options.aliases.length;t++)e.push(n.options.aliases[t].toLowerCase());for(o=r(n.fn,"function")?n.fn():n.fn,s=0;s<e.length;s++)i=e[s],l=i.split("."),1===l.length?Modernizr[l[0]]=o:(!Modernizr[l[0]]||Modernizr[l[0]]instanceof Boolean||(Modernizr[l[0]]=new Boolean(Modernizr[l[0]])),Modernizr[l[0]][l[1]]=o),C.push((o?"":"no-")+l.join("-"))}}function s(e){var n=_.className,t=Modernizr._config.classPrefix||"";if(b&&(n=n.baseVal),Modernizr._config.enableJSClass){var r=new RegExp("(^|\\s)"+t+"no-js(\\s|$)");n=n.replace(r,"$1"+t+"js$2")}Modernizr._config.enableClasses&&(n+=" "+t+e.join(" "+t),b?_.className.baseVal=n:_.className=n)}function i(e,n){return function(){return e.apply(n,arguments)}}function l(e,n,t){var o;for(var s in e)if(e[s]in n)return t===!1?e[s]:(o=n[e[s]],r(o,"function")?i(o,t||n):o);return!1}function a(e,n){return!!~(""+e).indexOf(n)}function u(){return"function"!=typeof n.createElement?n.createElement(arguments[0]):b?n.createElementNS.call(n,"http://www.w3.org/2000/svg",arguments[0]):n.createElement.apply(n,arguments)}function f(e){return e.replace(/([a-z])-([a-z])/g,function(e,n,t){return n+t.toUpperCase()}).replace(/^-/,"")}function d(e){return e.replace(/([A-Z])/g,function(e,n){return"-"+n.toLowerCase()}).replace(/^ms-/,"-ms-")}function c(n,t,r){var o;if("getComputedStyle"in e){o=getComputedStyle.call(e,n,t);var s=e.console;if(null!==o)r&&(o=o.getPropertyValue(r));else if(s){var i=s.error?"error":"log";s[i].call(s,"getComputedStyle returning null, its possible modernizr test results are inaccurate")}}else o=!t&&n.currentStyle&&n.currentStyle[r];return o}function p(){var e=n.body;return e||(e=u(b?"svg":"body"),e.fake=!0),e}function m(e,t,r,o){var s,i,l,a,f="modernizr",d=u("div"),c=p();if(parseInt(r,10))for(;r--;)l=u("div"),l.id=o?o[r]:f+(r+1),d.appendChild(l);return s=u("style"),s.type="text/css",s.id="s"+f,(c.fake?c:d).appendChild(s),c.appendChild(d),s.styleSheet?s.styleSheet.cssText=e:s.appendChild(n.createTextNode(e)),d.id=f,c.fake&&(c.style.background="",c.style.overflow="hidden",a=_.style.overflow,_.style.overflow="hidden",_.appendChild(c)),i=t(d,e),c.fake?(c.parentNode.removeChild(c),_.style.overflow=a,_.offsetHeight):d.parentNode.removeChild(d),!!i}function g(n,r){var o=n.length;if("CSS"in e&&"supports"in e.CSS){for(;o--;)if(e.CSS.supports(d(n[o]),r))return!0;return!1}if("CSSSupportsRule"in e){for(var s=[];o--;)s.push("("+d(n[o])+":"+r+")");return s=s.join(" or "),m("@supports ("+s+") { #modernizr { position: absolute; } }",function(e){return"absolute"==c(e,null,"position")})}return t}function y(e,n,o,s){function i(){d&&(delete T.style,delete T.modElem)}if(s=r(s,"undefined")?!1:s,!r(o,"undefined")){var l=g(e,o);if(!r(l,"undefined"))return l}for(var d,c,p,m,y,v=["modernizr","tspan","samp"];!T.style&&v.length;)d=!0,T.modElem=u(v.shift()),T.style=T.modElem.style;for(p=e.length,c=0;p>c;c++)if(m=e[c],y=T.style[m],a(m,"-")&&(m=f(m)),T.style[m]!==t){if(s||r(o,"undefined"))return i(),"pfx"==n?m:!0;try{T.style[m]=o}catch(h){}if(T.style[m]!=y)return i(),"pfx"==n?m:!0}return i(),!1}function v(e,n,t,o,s){var i=e.charAt(0).toUpperCase()+e.slice(1),a=(e+" "+z.join(i+" ")+i).split(" ");return r(n,"string")||r(n,"undefined")?y(a,n,o,s):(a=(e+" "+P.join(i+" ")+i).split(" "),l(a,n,t))}function h(e,n,r){return v(e,t,t,n,r)}var C=[],w=[],S={_version:"3.5.0",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(e,n){var t=this;setTimeout(function(){n(t[e])},0)},addTest:function(e,n,t){w.push({name:e,fn:n,options:t})},addAsyncTest:function(e){w.push({name:null,fn:e})}},Modernizr=function(){};Modernizr.prototype=S,Modernizr=new Modernizr;var _=n.documentElement,b="svg"===_.nodeName.toLowerCase(),x="Moz O ms Webkit",P=S._config.usePrefixes?x.toLowerCase().split(" "):[];S._domPrefixes=P;var z=S._config.usePrefixes?x.split(" "):[];S._cssomPrefixes=z;var E={elem:u("modernizr")};Modernizr._q.push(function(){delete E.elem});var T={style:E.elem.style};Modernizr._q.unshift(function(){delete T.style}),S.testAllProps=v,S.testAllProps=h,Modernizr.addTest("cssgridlegacy",h("grid-columns","10px",!0)),Modernizr.addTest("cssgrid",h("grid-template-rows","none",!0)),Modernizr.addTest("borderimage",h("borderImage","url() 1",!0)),o(),s(C),delete S.addTest,delete S.addAsyncTest;for(var N=0;N<Modernizr._q.length;N++)Modernizr._q[N]();e.Modernizr=Modernizr}(window,document);
-
-function burgerButton(x) { x.classList.toggle("change"); }
-function menuFocus() { document.querySelector(".burgerButton").click(); }
-function fadeoutright() { $('main').addClass("fadeoutright"); $('aside').addClass("fadeOut"); scrollup(); }
-function fadeoutleft() { $('main').addClass("fadeoutleft"); $('aside').addClass("fadeOut"); scrollup(); }
+function burgerButton(x) { x.classList.toggle('change'); }
+function menuFocus() { document.querySelector('.burgerButton').click(); }
+function fadeoutright() { $('main').removeClass('fadeinleft fadeinright').addClass('fadeoutright'); }
+function fadeoutleft() { $('main').removeClass('fadeinleft fadeinright').addClass('fadeoutleft'); }
+function fadeOut() { $('main').removeClass('fadeinleft fadeinright').addClass('fadeOut'); }
 function scrollup() { $('html, body').animate({scrollTop: 0}, 150); return false; }
-function someone() { $("body").addClass("someone"); } // help i've fallen and i cant get up
-function textNormal() {
+function someone() { $('body').addClass('someone'); } // help i've fallen and i cant get up
+function textNormal() { 
 	$('html').css("font-size", "1em"); 
-	$("a[href='index.html?textLarge=true']").attr('href', 'index.html');
-	$("a[href='news.html?textLarge=true']").attr('href', 'news.html');
+	document.cookie = "text=normal; path=/; expires=0;";
 }
-
-function textLarge() {
+function textLarge() { 
 	$('html').css("font-size", "2em"); 
-	$("a[href='index.html']").attr('href', 'index.html?textLarge=true');
-	$("a[href='news.html']").attr('href', 'news.html?textLarge=true');
+	document.cookie = "text=large; path=/; expires=0;";
 }
 
-function darkSideOfTheMoon() {
-	$('head').append('<link rel="stylesheet" id="dark" href="assets/dark.css" type="text/css"/>'); 
-	$("a[href='index.html']").attr('href', 'index.html?dark=hellyeah');
-	$("a[href='news.html']").attr('href', 'news.html?dark=hellyeah');
-}
+function darkSideOfTheMoon() { $('head').append('<link rel="stylesheet" id="dark" href="assets/dark.css" type="text/css"/>'); }
 
 $(document).ready(function() {
 	// add accessbility
-	$(".titleText").css("left", "2.8rem");
-	$(".title").append("<span class=\"accessibilityMenu\"><a class=\"captionButton textAdjust\" onclick=\"textNormal\(\)\" title=\"Disable Larger Text\">a</a> <a class=\"captionButton textAdjust\" onclick=\"textLarge\(\)\" title=\"Enable Larger Text\">A</a> <a class=\"captionButton contrast\" title=\"Toggle High Contrast Mode\"><i class=\"fas fa-adjust\"></i></a></span>");
+	if (navigator.cookieEnabled) {
+		$('.titleText').css('left', '2.8rem');
+		$('.title').append('<span class=\"accessibilityMenu\"><a class=\"captionButton textAdjust\" onclick=\"textNormal\(\)\" title=\"Disable Larger Text\">a</a> <a class=\"captionButton textAdjust\" onclick=\"textLarge\(\)\" title=\"Enable Larger Text\">A</a> <a class=\"captionButton contrast\" title=\"Toggle High Contrast Mode\"><i class=\"fas fa-adjust\"></i></a></span>');
+		console.log('s u c c e s s: your browser can eat cookies');
+	}
+	else { console.log('f a i l: your browser cannot eat cookies'); };
 
 //  Barba.Pjax.start();    // init barbra
 	Barba.Prefetch.init(); // init barbra
@@ -76,22 +70,23 @@ $(document).ready(function() {
 	Mousetrap.bind('m y s t e r i o u s space a s space t h e space d a r k space s i d e space o f space t h e space m o o n', function() { darkSideOfTheMoon(); });
 	Mousetrap.bind('up up down down left right left right b a enter', function() { darkSideOfTheMoon(); });
 	Mousetrap.bind('@ s o m e o n e', function() { someone(); });
+	Mousetrap.bind('n o space u', function() { $('#buttonlist-buttonhtml, #404-buttonhtml').toggle(); });
 
 	// detect microsoft edge and apply a hotfix CSS file (edgey mode)
 	var userAgent = navigator.userAgent;
 	if( userAgent.indexOf('Edge') >= 0 ) {
-		console.log("Microsoft, please fix border-image"); // no hard feelings
-		$("head").append("<link rel=\"stylesheet\" href=\"assets/edgey.css\" type=\"text/css\"/>");
+		console.log('Microsoft, please fix border-image'); // no hard feelings
+		$('head').append('<link rel=\"stylesheet\" href=\"assets/edgey.css\" type=\"text/css\"/>');
 	}
 
 	// Modernizr
-	if (Modernizr.cssgrid) { console.log("s u c c e s s: your browser can do CSS grids"); } 
-	else { console.log("f a i l: your browser sucks and cannot CSS grids correctly.. like is it that hard to install Chrome or something"); }
+	if (Modernizr.cssgrid) { console.log('s u c c e s s: your browser can do CSS grids'); } 
+	else { console.log('f a i l: your browser sucks and cannot CSS grids correctly.. like is it that hard to install Chrome or something'); }
 
-	if (Modernizr.borderimage) { console.log("s u c c e s s: your browser can do border-image"); } 
+	if (Modernizr.borderimage) { console.log('s u c c e s s: your browser can do border-image'); } 
 	else {
-		console.log("f a i l: your browser sucks and cannot border-image correctly"); // no hard feelings
-		$("head").append("<link rel=\"stylesheet\" href=\"assets/edgey.css\" type=\"text/css\"/>");
+		console.log('f a i l: your browser sucks and cannot border-image correctly'); // no hard feelings
+		$('head').append('<link rel=\"stylesheet\" href=\"assets/edgey.css\" type=\"text/css\"/>');
 	}
 
 	// burger button class toggle
@@ -119,46 +114,29 @@ $(document).ready(function() {
 		if(clickedContrast) {
 			clickedContrast = false;
 			$('#contrast').remove(); 
-			$("a[href='index.html?contrast=true']").attr('href', 'index.html');
-			$("a[href='news.html?contrast=true']").attr('href', 'news.html');
+			document.cookie = "contrast=false; path=/; expires=0;";
 		}
 		else {
 			clickedContrast = true;
 			$('head').append('<link rel="stylesheet" id="contrast" href="assets/contrast.css" type="text/css"/>');
-			$("a[href='index.html']").attr('href', 'index.html?contrast=true');
-			$("a[href='news.html']").attr('href', 'news.html?contrast=true');
-
+			document.cookie = "contrast=true; path=/; expires=0;";
 		}
 	});
 
-	// data query thingamadoohickers
-	if (window.location.href.includes("contrast=true")) {
-	 	clickedContrast = true; 
-		$('head').append('<link rel="stylesheet" id="contrast" href="assets/contrast.css" type="text/css"/>'); 
-		$("a[href='index.html']").attr('href', 'index.html?contrast=true');
-		$("a[href='news.html']").attr('href', 'news.html?contrast=true');
-	}
-	else { $('#contrast').remove(); }
-
-	if (window.location.href.includes("textLarge=true")) { textLarge(); }
-	else { textNormal(); }
-
-	if (window.location.href.includes("dark=hellyeah")) { darkSideOfTheMoon() }
-	else { $('#dark').remove(); }
+	// apply existing settings if they exist	
+	if (document.cookie.includes('text=large')) { textLarge(); }
+	if (document.cookie.includes('contrast=true')) { $('.contrast').click(); }
 
 	// delay links - https://stackoverflow.com/questions/8775541/delay-a-link-click (MIT)
-	$("a.delayLink[href]").click(function(){
+	$('a.delayLink[href]').click(function(){
 		var self = $(this);
 		setTimeout(function() {
 			window.location.href = self.attr('href'); // go to href after the slide animation completes
-		}, 300);
+		}, 400);
 		return false; // And also make sure you return false from your click handler.
 	});
 });
 
 // animationals WAIT FOR CNAME 
-$("#gitbutton").click(function() {$('.github').toggleClass('github-active');});
-$("#discordbutton").click(function() {$('.discord').toggleClass('discord-active');});
-
-// delay links when animationals come
-function delay (URL) {setTimeout( function() {window.location = URL;}, 30) ;}
+$('#gitbutton').click(function() { $('.github').toggleClass('active'); });
+$('#discordbutton').click(function() { $('.discord').toggleClass('active'); });
