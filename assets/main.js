af30aa186dfe705a16a4c7a6ab68ce058209aeed
logonoff <logonoff@users.noreply.github.com>
2019-08-02 23:14:06 -0400

    highlight current section in docs

--- assets/main.js
+++ assets/main.js
@@ -294,12 +294,50 @@ function onKonamiCode(cb) {
 }
 
 /*
+** Docs
+*/
+// Cache selectors
+var lastId,
+	topMenu = $(".toc ul"),
+	topMenuHeight = topMenu.outerHeight()+1,
+	// All list items
+	menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+	scrollItems = menuItems.map(function(){
+		var item = $($(this).attr("href"));
+		if (item.length) { return item; }   
+	});
+
+// Bind to scroll
+$(window).scroll(function(){
+	// Get container scroll position
+	var fromTop = $(this).scrollTop()+topMenuHeight;
+
+	// Get id of current scroll item
+	var cur = scrollItems.map(function(){
+		if ($(this).offset().top < fromTop)
+			return this;
+	});
+	// Get the id of the current element
+	cur = cur[cur.length-1];
+	var id = cur && cur.length ? cur[0].id : "";
+
+	if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+				.parent().removeClass("current")
+				.end().filter("[href='#"+id+"']").parent().addClass("current");
+	}
+});
+
+/*
 ** rgb color cycle
 ** adapted from https://codepen.io/SJF/pen/wBdpXV
 */
-var r = 255; // pretty sure these have to be global
-var g = 0;
-var b = 0;
+var r = 255, // pretty sure these have to be global
+	g = 0,
+	b = 0;
 
 function rainbowz() {
 	/*
